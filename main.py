6b007052a866867d8afc8f2d9bb876ac2523bd95
harrywhoforks <phwongharris@gmail.com>
2019-01-31 18:19:17 +0800

    Improve handling no all surfaces in csv files condition

--- main.py
+++ main.py
@@ -121,8 +121,7 @@ def choose_direction(data, average):
     for direction, data_surfaces in data_direction_surfaces:
         if all(average_surface in data_surfaces for average_surface in average_surfaces):
             return direction
-    print("None of the csv file contains all surfaces. This just does not work here...")
-    quit()
+    return None
            
 if __name__ == "__main__":
     factors = get_factors()
@@ -130,7 +129,10 @@ if __name__ == "__main__":
     data = read(files)
     average = process(data, factors)
     the_chosen_one = choose_direction(data, average)
-    files = {file_name: direction for file_name, direction in files.items() if direction == the_chosen_one}
-    for file in files:
-        write(file, average)
+    if the_chosen_one == None:
+        print("None of the csv file contains all surfaces. This just does not work here...")
+    else:
+        files = {file_name: direction for file_name, direction in files.items() if direction == the_chosen_one}
+        for file in files:
+            write(file, average)
     input("Process have reached the end.")
